5ed22d65d366d39180c2c94b15c4304e555726d0
romalitv <youlikeyoupass@gmail.com>
2023-11-21 09:42:09 +0200

    added basic logic

--- src/main/kotlin/com/bcknd/Application.kt
+++ src/main/kotlin/com/bcknd/Application.kt
@@ -1,5 +1,15 @@
 package com.bcknd
 
-fun main() {
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.bcknd.plugins.*
 
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
 }

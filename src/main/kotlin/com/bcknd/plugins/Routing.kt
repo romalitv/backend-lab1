23ea42e06aa2838311c8a99013de5d386548e85f
romalitv <youlikeyoupass@gmail.com>
2023-11-22 20:23:31 +0200

    added healthcheck endpoint :)

--- src/main/kotlin/com/bcknd/plugins/Routing.kt
+++ src/main/kotlin/com/bcknd/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.bcknd.plugins
 
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -8,6 +9,10 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Simple build")
+        }
+
+        get("/healthcheck") {
+            call.respond(HttpStatusCode.OK, "Status 200")
         }
     }
 }
